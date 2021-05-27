11356e1460fcc98d39816c94f5a165e43f3429b6
TranNhi123 <73085711+TranNhi123@users.noreply.github.com>
2021-05-27 23:38:44 +0700

    fixlibary

--- client/src/components/pages/LoginForm.js
+++ client/src/components/pages/LoginForm.js
@@ -65,7 +65,7 @@ class LoginForm extends Component {
                 <a className="login-form-forgot" href="/#">
                   Forgot your password
                 </a>
-                <Link to="/registrationDetail/generationInformation"><button onClick={this.confirmLogin} id="btn-login">Log in</button></Link>
+                <Link to="/AddHomeBlock"><button onClick={this.confirmLogin} id="btn-login">Log in</button></Link>
                 
               </div>
               <div className="line-spacing"></div>
